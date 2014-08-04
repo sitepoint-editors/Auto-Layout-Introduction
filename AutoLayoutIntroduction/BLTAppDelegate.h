fa6a65c59cf9f58b33418e59e3f7103b7dfd3059
Chris Ward <chriswhward@gmail.com>
2014-08-04 15:58:40 +0200

    Initial commit

---
+++ AutoLayoutIntroduction/BLTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BLTAppDelegate.h
+//  AutoLayoutIntroduction
+//
+//  Created by Ben Taylor on 28/07/2014.
+//  Copyright (c) 2014 Ben Taylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AutoLayoutIntroduction/BLTViewController.m
@@ -0,0 +1,29 @@
+//
+//  BLTViewController.m
+//  AutoLayoutIntroduction
+//
+//  Created by Ben Taylor on 28/07/2014.
+//  Copyright (c) 2014 Ben Taylor. All rights reserved.
+//
+
+#import "BLTViewController.h"
+
+@interface BLTViewController ()
+
+@end
+
+@implementation BLTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
